6a18aed3c16fb9f9510dcc4aeb4674ffbc78ee86
g7heimm <marc.heimann@swisslog.com>
2020-06-22 09:36:03 +0200

    changes prepared

--- src/it/java/com/vanderlande/demo/DemoSCSSteps.java
+++ src/it/java/com/vanderlande/demo/DemoSCSSteps.java
@@ -28,7 +28,7 @@ public class DemoSCSSteps {
 
 	private String jsonInputString;
 	private String apiVersion = "v1";
-	private String serverUri = "http://asn.vi-application/"+apiVersion+"/asns/";	
+	private String serverUri = "http://asn.multi/"+apiVersion+"/asns/";	
 	
 	@Before
 	public void before() {	
